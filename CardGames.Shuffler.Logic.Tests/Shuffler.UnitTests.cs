b862b2a4fb175a34cdeb44064c17562c2261284a
Andrew Webster <andrew.webster@rocketmail.com>
2017-08-14 19:47:12 -0700

    Final version of card shuffler with two unit tests

--- CardGames.Shuffler.Logic.Tests/Shuffler.UnitTests.cs
+++ CardGames.Shuffler.Logic.Tests/Shuffler.UnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace CardGames.Shuffler.Logic.Tests
 {
@@ -6,7 +7,7 @@ namespace CardGames.Shuffler.Logic.Tests
     public class ShufflerUnitTests
     {
         [TestMethod]
-        public void ShuffleShouldRandomizeADeckOfCards()
+        public void ShuffleShouldMoveAces()
         {
             // arrange
             var shuffler = new Shuffler();
@@ -19,6 +20,34 @@ namespace CardGames.Shuffler.Logic.Tests
             Assert.AreNotEqual("AD", deck.Cards[13]);
             Assert.AreNotEqual("AC", deck.Cards[26]);
             Assert.AreNotEqual("AS", deck.Cards[39]);
+        }
+
+        [TestMethod]
+        public void ShuffleShouldRandomizeADeckOfCards()
+        {
+            // arrange
+            var unshuffledDeck = new List<string>
+            {
+                "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
+                "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD",
+                "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
+                "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS"
+            };
+            var shuffler = new Shuffler();
+
+            // act
+            var deck = shuffler.Shuffle();
+
+            // assert
+            var numberOfMatches = 0;
+            for (var i = 0; i < 52; i++)
+            {
+                if (unshuffledDeck[i] == deck.Cards[i] || (i > 0 && deck.Cards[i-1] == unshuffledDeck[i]) || (i < 51 && deck.Cards[i + 1] == unshuffledDeck[i]))
+                {
+                    numberOfMatches++;
+                }
+            }
+            Assert.IsTrue(numberOfMatches < 5, $"{numberOfMatches} is not less than 5");
         }
     }
 }
--- CardGames.Shuffler.Logic/Shuffler.cs
+++ CardGames.Shuffler.Logic/Shuffler.cs
@@ -1,13 +1,39 @@
 ﻿using System;
 using CardGames.Shuffler.Model;
+using System.Collections.Generic;
 
 namespace CardGames.Shuffler.Logic
 {
     public class Shuffler : IShuffleCards
     {
+        private readonly Dictionary<int, string> _unshuffledDeck = new Dictionary<int, string>
+        {
+            {1, "AH"}, {2, "2H"}, {3, "3H"}, {4, "4H"}, {5, "5H"}, {6, "6H"}, {7, "7H"}, {8, "8H"}, {9, "9H"}, {10, "10H"}, {11, "JH"}, {12, "QH"}, {13, "KH"},
+            {14, "AD"}, {15, "2D"}, {16, "3D"}, {17, "4D"}, {18, "5D"}, {19, "6D"}, {20, "7D"}, {21, "8D"}, {22, "9D"}, {23, "10D"}, {24, "JD"}, {25, "QD"}, {26, "KD"},
+            {27, "AC"}, {28, "2C"}, {29, "3C"}, {30, "4C"}, {31, "5C"}, {32, "6C"}, {33, "7C"}, {34, "8C"}, {35, "9C"}, {36, "10C"}, {37, "JC"}, {38, "QC"}, {39, "KC"},
+            {40, "AS"}, {41, "2S"}, {42, "3S"}, {43, "4S"}, {44, "5S"}, {45, "6S"}, {46, "7S"}, {47, "8S"}, {48, "9S"}, {49, "10S"}, {50, "JS"}, {51, "QS"}, {52, "KS"}
+        };
+
         public Deck Shuffle()
         {
-            throw new NotImplementedException();
+            var deck = new Deck
+            {
+                Cards = new List<string>(),
+                IsShuffled = false
+            };
+
+            while (deck.Cards.Count < 52)
+            {
+                var random = new Random();
+                var position = random.Next(1, 53);
+
+                if (!deck.Cards.Contains(_unshuffledDeck[position]))
+                {
+                    deck.Cards.Add(_unshuffledDeck[position]);
+                }
+            }
+
+            return deck;
         }
     }
 }
